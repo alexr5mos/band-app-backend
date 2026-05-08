1f9d138437555e75dbe3429fabab15a3a7b8ec91
Alex <alexr5mos@gmail.com>
2026-05-08 21:33:01 +0100

    Explicitly list Authorization in CORS headers

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const app = express();
 // CORS - must be first
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
   res.setHeader('Access-Control-Max-Age', '86400');
   if (req.method === 'OPTIONS') {
     res.status(204).end();
